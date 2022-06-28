a023a51996c5815a935f6bc27862703ca08d3cca
Liu <76809218+lioujg@users.noreply.github.com>
2022-06-28 19:15:59 +0800

    adjust the xd

--- test.m
+++ test.m
@@ -6,6 +6,13 @@ d = c * 2;
 e = zeros(3, 1);
 
 % c_sum = sum(c, 2)
-size(c, 2)
+% size(c, 2)
+
+
+mass = 0.27;
+center_of_mass = [0 0 0];
+inertia_matrix = [0.000204525 0 0 4.05e-06 0 0.000204525];
+r = -0.15;
+theta_o = [mass mass*center_of_mass inertia_matrix r].';
 
 
